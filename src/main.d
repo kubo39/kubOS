2d6110ddc9554f1c459b1f0f48fd17d274ac1842
kubo39 <kubo39@gmail.com>
2015-11-19 20:47:18 +0900

    Print Hello, World!

--- src/main.d
+++ src/main.d
@@ -1,4 +1,4 @@
-import std.range;
+import vgabuffer;
 
 @system:
 
@@ -6,19 +6,6 @@ extern(C):
 
 void d_main() @nogc nothrow
 {
-  char[13] hello = ['H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!'];
-  ubyte colorByte = 0x1f;  // white foreground, blue background
-  ubyte[24] helloColored = [
-    colorByte, colorByte, colorByte, colorByte, colorByte, colorByte,
-    colorByte, colorByte, colorByte, colorByte, colorByte, colorByte,
-    colorByte, colorByte, colorByte, colorByte, colorByte, colorByte,
-    colorByte, colorByte, colorByte, colorByte, colorByte, colorByte];
-
-  foreach (i, c; hello) {
-    helloColored[i*2] = c;
-  }
-
-  ulong* bufferPtr = cast(ulong*)(0xb8000 + 1988);
-  *bufferPtr = cast(ulong)(helloColored.ptr);
+  printHello();
   for(;;){}
 }
