ce5e23a5359753fe24d71a32dd4881a42aa641c8
kubo39 <kubo39@gmail.com>
2015-11-22 19:23:50 +0900

    Untabify

--- src/util.d
+++ src/util.d
@@ -3,21 +3,22 @@ immutable ubyte[] BASE_CHARS = cast(immutable ubyte[])"0123456789ABCDEF";
 
 size_t itoa(int value, ubyte* buf, size_t len, uint base = 10) nothrow @nogc
 {
-	assert(1 < base && base <= 16);
-	size_t pos = len;
-	bool sign = false;
+  assert(1 < base && base <= 16);
+  size_t pos = len;
+  bool sign = false;
 
-	if (value < 0) {
-		sign = true;
-		value = -value;
-	}
+  if (value < 0) {
+    sign = true;
+    value = -value;
+  }
 
-	do {
-		buf[--pos] = BASE_CHARS[value % base];
-	} while (value /= base);
+  do {
+    buf[--pos] = BASE_CHARS[value % base];
+  } while (value /= base);
 
-	if (sign)
-		buf[--pos] = '-';
+  if (sign) {
+    buf[--pos] = '-';
+  }
 
-	return pos;
+  return pos;
 }
